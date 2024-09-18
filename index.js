6f4972381abe61a5ac3cc8da9a8aff46ee0263fc
馬苡瑄 <cindyma@mayixuande-MacBook-Pro.local>
2024-09-18 20:58:18 +0800

    CSS and JavaScript

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const sideMenu = document.getElementById('sideMenu');
+    const overlay = document.getElementById('overlay');
+    sideMenu.classList.toggle('active');
+    overlay.classList.toggle('active');
+}
